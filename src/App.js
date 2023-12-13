84ebfeb1e7d8f9bfc5ec719acbc305c46dfd5571
Anna Gerhardt <annagerhardt@Annas-MBP.lan>
2023-12-12 22:14:14 -0500

    lesson 1.5 Dide-effects, custom hooks

--- src/App.js
+++ src/App.js
@@ -2,20 +2,35 @@ import React, { useState } from "react";
 import TodoList from "./TodoList";
 import AddTodoForm from "./AddTodoForm.js";
 
+//custom hook
+const useSemiPersistentState = (initialState, key) => {
+  // Default state to read "savedTodoList" from localStorage
+  const [state, setState] = useState(
+    JSON.parse(localStorage.getItem(key)) || initialState
+  );
+  
+  React.useEffect(() => {
+    // Save todoList to localStorage with the key "savedTodoList"
+    localStorage.setItem(key, JSON.stringify(state));
+    }, [state, key]);
+
+    return [state, setState]
+}
+
 function App() {
-  const [todoList, setTodoList] = useState([]);
+  const [todoList, setTodoList] = useSemiPersistentState([], "savedTodoList");
 
   const addTodo = (newTodo) => {
     setTodoList([...todoList, newTodo]);
   };
 
   return (
-    <div>
+    <>
       <h1>ToDo List</h1>
       <AddTodoForm onAddTodo={addTodo} />
 
       <TodoList todoList={todoList} />
-    </div>
+    </>
   );
 }
 
--- src/TodoList.js
+++ src/TodoList.js
@@ -1,21 +1,6 @@
 import React from "react";
 import TodoListItem from "./TodoListItem";
 
-// const todoList = [
-//   {
-//     id: 1,
-//     title: "Wake up",
-//   },
-//   {
-//     id: 2,
-//     title: "Take a shower",
-//   },
-//   {
-//     id: 3,
-//     title: "Walk the dog",
-//   },
-// ];
-
 const TodoList = ({todoList}) => {
   return (
     <ul>
